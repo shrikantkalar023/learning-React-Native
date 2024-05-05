667fe5837d125f630453a92a9233c15f2e7a32de
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-05 17:21:23 +0530

    Text component

--- App.tsx
+++ App.tsx
@@ -3,7 +3,11 @@ import { SafeAreaView, StyleSheet, Text } from "react-native";
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text numberOfLines={2} onPress={() => console.log("Text pressed")}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit laborum
+        perspiciatis eum voluptate dolore cum quaerat perferendis nemo iure modi
+        velit, ad quam laboriosam libero.
+      </Text>
     </SafeAreaView>
   );
 }
