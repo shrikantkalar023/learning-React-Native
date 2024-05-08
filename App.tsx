a6b3d0d1a10bb657a31f49d8e46c6dfbc1bf365d
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-08 17:28:13 +0530

    button component

--- App.tsx
+++ App.tsx
@@ -1,33 +1,13 @@
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { Button, SafeAreaView, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} onPress={() => console.log("Text pressed")}>
-        Hello, World!
-      </Text>
-      {/* <Image source={require("./assets/splash.png")} /> */}
-      <TouchableNativeFeedback onPress={() => console.log("Image pressed")}>
-        {/* <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        /> */}
-        <View
-          style={{ width: 100, height: 100, backgroundColor: "dodgerblue" }}
-        ></View>
-      </TouchableNativeFeedback>
+      <Button
+        title="Click Me"
+        onPress={() => console.log("button clicked")}
+        color="blue"
+      />
     </SafeAreaView>
   );
 }
