b8305f4acfc7bd5134c900279a39df63c7d58a2e
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-13 20:22:15 +0530

    Platform specific code

--- App.tsx
+++ App.tsx
@@ -1,31 +1,21 @@
-import { Alert, Button, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("title", "message", [
-            { text: "Yes", onPress: () => console.log("Yes Pressed") },
-            {
-              text: "No",
-              onPress: () => console.log("No Pressed"),
-            },
-          ])
-        }
-        color="blue"
-      />
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" color="blue" />
     </SafeAreaView>
   );
 }
 
-const containerStyle = { backgroundColor: "yellow" };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
