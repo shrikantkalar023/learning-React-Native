32261bf3659d79921ff4da96521d51475f44c07e
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-05-19 18:09:23 +0530

    flex direction

--- App.tsx
+++ App.tsx
@@ -4,14 +4,15 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "red",
+        backgroundColor: "white",
         flex: 1, // takes entire screen. .5 would take half
+        flexDirection: "row", // default is column
       }}
     >
       {/* flex: 2 takes 2/4 of the container below */}
-      <View style={{ backgroundColor: "blue", flex: 2 }} />
-      <View style={{ backgroundColor: "gold", flex: 1 }} />
-      <View style={{ backgroundColor: "tomato", flex: 1 }} />
+      <View style={{ backgroundColor: "blue", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
 }
--- README.md
+++ README.md
@@ -23,3 +23,5 @@
 12. **SafeAreaView** only works on ios. Add padding = StatusBar.currentHeight on android.
 
 13. **flex**: to divide the space in the container. _flex: 1_ takes the entire space. Inside a container, if we have 2 children, and we set _flex: 1_ on both, they will take equal space. If we set _flex: 2_ on one and _flex: 1_ on the other, the first one will take 2/3 and the second one will take 1/3 of the space.
+
+14. **flexDirection**: default is column.
