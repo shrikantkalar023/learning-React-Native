ed9d45c3f3933447521928b70ccf0b12c2e6c396
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-06-02 20:04:33 +0530

    switch component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import { Switch, Text } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [isNew, setIsNew] = useState(false);
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Screen>
-        <AppTextInput icon="email" placeholder="First Name" />
+        <Switch
+          value={isNew}
+          onValueChange={(newValue) => setIsNew(newValue)}
+        />
+        <Text>{isNew ? "true" : "false"}</Text>
       </Screen>
     </GestureHandlerRootView>
   );
