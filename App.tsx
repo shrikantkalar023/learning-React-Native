025a683a0dda8196f7a801e7be7ce5e26e841743
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-06-08 02:43:20 +0530

    ADD: image picker native component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Button, Image } from "react-native";
+import Screen from "./app/components/Screen";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState<string>();
+
+  const selectImage = async () => {
+    // No permissions request is necessary for launching the image library
+
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) {
+        setImageUri(result.assets[0].uri);
+      }
+    } catch (error) {
+      console.log("Error selecting image", error);
+    }
+  };
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingEditScreen />
-    </GestureHandlerRootView>
+    <Screen style={{ flex: 1 }}>
+      <Button title="Pick an image from camera roll" onPress={selectImage} />
+      {imageUri && (
+        <Image
+          source={{ uri: imageUri }}
+          style={{
+            width: 200,
+            height: 200,
+          }}
+        />
+      )}
+    </Screen>
   );
 }
