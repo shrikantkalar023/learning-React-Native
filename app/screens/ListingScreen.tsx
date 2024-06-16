251adfdee00e85ce2610ce5186f870c2734cc214
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-06-16 23:07:59 +0530

    handling network errors

--- app/screens/ListingScreen.tsx
+++ app/screens/ListingScreen.tsx
@@ -1,7 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
+
 import listingsApi from "../api/listings";
+import AppButton from "../components/AppButton";
+import AppText from "../components/AppText";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
@@ -13,9 +16,13 @@ interface Props
 
 const ListingScreen = ({ navigation }: Props) => {
   const [listings, setListings] = useState<IListing[]>([]);
+  const [error, setError] = useState(false);
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true);
+
+    setError(false); // Reset error state
     response.data && setListings(response.data);
   };
 
@@ -25,6 +32,12 @@ const ListingScreen = ({ navigation }: Props) => {
 
   return (
     <Screen style={styles.screen}>
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <AppButton title="Retry" onPress={loadListings} />
+        </>
+      )}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
