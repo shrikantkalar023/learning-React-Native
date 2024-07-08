bfc4542979ec8a3c95cb0357afbb504d408eea8e
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-08 18:31:50 +0530

    netinfo api to detect network

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import { useNetInfo } from "@react-native-community/netinfo";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
-export default function App() {
+export default async function App() {
+  const netInfo = useNetInfo();
+  console.log(netInfo);
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <GestureHandlerRootView>
