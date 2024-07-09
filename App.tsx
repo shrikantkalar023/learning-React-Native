6cdded9d249be3401b49a281853bf6068f7baf32
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-09 18:30:18 +0530

    async storage demo

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
-import { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
-export default async function App() {
-  const netInfo = useNetInfo();
-  console.log(netInfo);
+export default function App() {
+  const asyncStorageDemo = async () => {
+    try {
+      await AsyncStorage.setItem(
+        "person",
+        JSON.stringify({ id: 1, name: "John" })
+      );
+
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value as string);
+
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  asyncStorageDemo();
 
   return (
     <NavigationContainer theme={navigationTheme}>
