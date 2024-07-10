0ee08b9a3dabbc9d26acdf57bdc63080924e9c03
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-10 21:41:28 +0530

    cache layer

---
+++ app/utility/cache.ts
@@ -0,0 +1,45 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const prefix = "cached";
+const expiryInMilliseconds = 5 * 60 * 1000;
+
+const store = async (key: string, value: unknown) => {
+  try {
+    const item = {
+      value,
+      timestamp: Date.now(),
+    };
+
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const isExpired = (item: { timestamp: number }) => {
+  return Date.now() - item.timestamp > expiryInMilliseconds;
+};
+
+const get = async (key: string) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    if (!value) return;
+
+    const item: {
+      value: unknown;
+      timestamp: number;
+    } = JSON.parse(value);
+
+    // Command Query Separation (CQS) principle is violated here
+    if (isExpired(item)) {
+      await AsyncStorage.removeItem(prefix + key);
+      return;
+    }
+
+    return item.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default { store, get };
