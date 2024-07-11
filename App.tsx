0b7fceceddbd409075ed24c06115dd1f9f17767c
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-11 15:50:50 +0530

    extend api client to include caching

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
@@ -6,23 +5,6 @@ import AppNavigator from "./app/navigation/AppNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
-  const asyncStorageDemo = async () => {
-    try {
-      await AsyncStorage.setItem(
-        "person",
-        JSON.stringify({ id: 1, name: "John" })
-      );
-
-      const value = await AsyncStorage.getItem("person");
-      const person = JSON.parse(value as string);
-
-      console.log(person);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  asyncStorageDemo();
-
   return (
     <NavigationContainer theme={navigationTheme}>
       <GestureHandlerRootView>
--- app/api/client.ts
+++ app/api/client.ts
@@ -1,7 +1,27 @@
-import { create } from "apisauce";
+import { ApiResponse, create } from "apisauce";
+import { AxiosRequestConfig } from "axios";
+import cache from "../utility/cache";
 
 const apiClient = create({
   baseURL: "http://localhost:9000/api",
 });
 
 export default apiClient;
+
+const get = apiClient.get;
+apiClient.get = async <T, U = T>(
+  url: string,
+  params?: {} | undefined,
+  axiosConfig?: AxiosRequestConfig<any> | undefined
+): Promise<ApiResponse<T, U>> => {
+  const response: ApiResponse<T, U> = await get(url, params, axiosConfig);
+  if (response.ok) {
+    cache.store(url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+  return data
+    ? { ok: true, data: data as T, originalError: null, problem: null }
+    : response;
+};
