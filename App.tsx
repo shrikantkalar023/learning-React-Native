0cf6312c1411caa94480ef93ce9560336da85587
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-19 18:32:57 +0530

    splash screen control

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
 import { jwtDecode } from "jwt-decode";
 import { useEffect, useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -12,22 +13,32 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
   const [user, setUser] = useState<IUser | null>(null);
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-
-    setUser(jwtDecode(token));
+    try {
+      const token = await authStorage.getToken();
+      if (token) setUser(jwtDecode(token));
+    } catch (error) {
+      console.log("Error restoring token", error);
+    } finally {
+      setIsReady(true);
+      await SplashScreen.hideAsync();
+    }
   };
 
   useEffect(() => {
     restoreToken();
   }, []);
 
+  if (!isReady) return null;
+
   return (
     <NavigationContainer theme={navigationTheme}>
-      <GestureHandlerRootView>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         <AuthContext.Provider value={{ user, setUser }}>
           <OfflineNotice />
           {user ? <AppNavigator /> : <AuthNavigator />}
