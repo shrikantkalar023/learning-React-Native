f9c727682061e4e02c6eaea5fe2c0cf8af194562
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-07-26 01:29:34 +0530

    expo push notifications

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -1,7 +1,11 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
+import { useEffect } from "react";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import AccountNavigator from "./AccountNavigator";
 import FeedNavigator from "./FeedNavigator";
@@ -14,40 +18,87 @@ export type AppNavigatorParams = {
 };
 
 const Tab = createBottomTabNavigator<AppNavigatorParams>();
-const AppNavigator = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }}>
-    <Tab.Screen
-      name="Feed"
-      component={FeedNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-      }}
-    ></Tab.Screen>
-    <Tab.Screen
-      name="ListingEdit"
-      component={ListingEditScreen}
-      options={({
-        navigation,
-      }: NativeStackScreenProps<AppNavigatorParams, "ListingEdit">) => ({
-        tabBarButton: () => (
-          <NewListingButton
-            onPress={() => navigation.navigate("ListingEdit")}
-          />
-        ),
-      })}
-    ></Tab.Screen>
-    <Tab.Screen
-      name="Account"
-      component={AccountNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    ></Tab.Screen>
-  </Tab.Navigator>
-);
+const AppNavigator = () => {
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    let token: string;
+    if (!Device.isDevice) return;
+
+    try {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        alert("Failed to get push token for push notification!");
+        return;
+      }
+
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ??
+          Constants?.easConfig?.projectId;
+        if (!projectId) {
+          throw new Error("Project ID not found");
+        }
+        token = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(token);
+      } catch (e) {
+        console.log("Error in inner try-catch block", e);
+      }
+    } catch (error) {
+      console.log("Error getting a push token", error);
+    }
+  };
+
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Feed"
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="ListingEdit"
+        component={ListingEditScreen}
+        options={({
+          navigation,
+        }: NativeStackScreenProps<AppNavigatorParams, "ListingEdit">) => ({
+          tabBarButton: () => (
+            <NewListingButton
+              onPress={() => navigation.navigate("ListingEdit")}
+            />
+          ),
+        })}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      ></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
 
 export default AppNavigator;
